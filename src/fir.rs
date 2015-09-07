0345ae17259da8edaee2bb544c9982db96563cb2
Adam Greig <adam@adamgreig.com>
2015-09-07 05:56:01 +0100

    add interpolation via polyphasing to FIR filter, closes #9

--- src/fir.rs
+++ src/fir.rs
@@ -7,14 +7,15 @@ use std::f64::consts::PI;
 /// (i.e. you can add a SampleType to an AccType), while the TapType should
 /// always be scalar and is usually the same size as the AccType.
 ///
-/// tap_sum() specifies the gain of the filter, i.e. the sum of all the taps.
+/// gain() specifies the gain of the filter, i.e. the sum of all the taps,
+/// divided by the interpolation level L.
 /// input() copies an item from the input into a given delay line element.
 /// accumulate() updates an accumulator with a delay line element * a tap.
 /// output() writes to the output array from an accumulator.
 pub trait SampleType: Copy {
     type AccType: Zero + Copy;
-    type TapType: Num + NumCast + Copy;
-    fn tap_sum() -> Self::TapType;
+    type TapType: Num + NumCast + Zero + Copy;
+    fn gain(interpolate: usize) -> Self::TapType;
     unsafe fn input(x: *const Self, delay: *mut Self::AccType);
     unsafe fn accumulate(acc: &mut Self::AccType, delay: *const Self::AccType,
                          tap: *const Self::TapType);
@@ -31,7 +32,7 @@ macro_rules! impl_scalar_sampletype {
             type TapType = $tt;
             
             #[inline]
-            fn tap_sum() -> $tt { $tapsum }
+            fn gain(interpolate: usize) -> $tt { $tapsum / interpolate as $tt}
             
             #[inline]
             unsafe fn input(x: *const $t, delay: *mut $tt) {
@@ -61,7 +62,7 @@ macro_rules! impl_complex_sampletype {
             type TapType = $tt;
 
             #[inline]
-            fn tap_sum() -> $tt { $tapsum }
+            fn gain(interpolate: usize) -> $tt { $tapsum / interpolate as $tt}
 
             #[inline]
             unsafe fn input(x: *const Complex<$t>, delay: *mut Complex<$tt>) {
@@ -102,39 +103,64 @@ impl_sampletype!(f32, f64, 1.0);
 /// FIR filter.
 pub struct FIR<T: SampleType> {
     taps: Vec<T::TapType>,
+    tap_idx: isize,
     delay: Vec<T::AccType>,
+    delay_idx: isize,
     decimate: usize,
-    delay_idx: isize
+    interpolate: usize,
 }
 
 impl <T: SampleType> FIR<T> {
     /// Create a new FIR with the given taps and decimation.
     ///
-    /// Taps should sum to T::tap_sum() or close to it.
+    /// Taps should sum to T::gain() or close to it.
     ///
     /// Set decimate=1 for no decimation, decimate=2 for /2, etc.
-    pub fn new(taps: &Vec<T::TapType>, decimate: usize) -> FIR<T>
+    /// Set interpolate=1 for no interpolation, interplate=2 for *2, etc.
+    /// Note that if the number of taps is not a multiple of the interpolation
+    /// ratio then they will be zero-padded at the end until they are.
+    ///
+    /// Implements a polyphase FIR to do efficient decimation and interpolation
+    /// (identical to a standard FIR when interpolate=1).
+    pub fn new(taps: &Vec<T::TapType>, decimate: usize, interpolate: usize)
+        -> FIR<T>
     {
         assert!(taps.len() > 0);
-        let taps: Vec<T::TapType> = taps.to_owned();
-        let mut delay: Vec<T::AccType> = Vec::with_capacity(taps.len());
-        for _ in 0..taps.len() {
+        assert!(decimate > 0);
+        assert!(interpolate > 0);
+
+        // Copy the taps and zero-pad to get a multiple of interpolation ratio
+        let mut taps: Vec<T::TapType> = taps.to_owned();
+        if taps.len() % interpolate != 0 {
+            for _ in 0..(interpolate - (taps.len() % interpolate)) {
+                taps.push(T::TapType::zero());
+            }
+        }
+
+        // Set up and fill the delay line with zeros
+        let n_delay = taps.len() / interpolate;
+        let mut delay: Vec<T::AccType> = Vec::with_capacity(n_delay);
+        for _ in 0..n_delay {
             delay.push(T::AccType::zero());
         }
-        FIR { taps: taps, delay: delay,
-              decimate: decimate, delay_idx: 0isize }
+
+        FIR { taps: taps, tap_idx: interpolate as isize - 1isize,
+              delay: delay, delay_idx: 0isize,
+              decimate: decimate, interpolate: interpolate }
     }
 
     /// Create a new FIR from a number of taps, desired frequency response
     /// (a 512-long vector from 0 to Nyquist freq) and decimation.
     ///
     /// Set decimate=1 for no decimation, decimate=2 for /2, etc.
-    pub fn from_gains(n_taps: usize, gains: &Vec<f64>, decimate: usize)
+    /// Set interpolate=1 for no interpolation, interplate=2 for *2, etc.
+    pub fn from_gains(n_taps: usize, gains: &Vec<f64>,
+                      decimate: usize, interpolate: usize)
         -> FIR<T>
     {
         let taps = firwin2(n_taps, gains);
-        let taps = quantise_taps::<T::TapType>(&taps, T::tap_sum());
-        FIR::new(&taps, decimate)
+        let taps = quantise_taps::<T::TapType>(&taps, T::gain(interpolate));
+        FIR::new(&taps, decimate, interpolate)
     }
 
     /// Create a new FIR that compensates for a CIC filter specified by
@@ -163,7 +189,7 @@ impl <T: SampleType> FIR<T> {
                 h[n] = 0.0_f64;
             }
         }
-        FIR::from_gains(n_taps, &h, decimate) 
+        FIR::from_gains(n_taps, &h, decimate, 1) 
     }
 
     /// Return a reference to the filter's taps.
@@ -176,37 +202,55 @@ impl <T: SampleType> FIR<T> {
     pub fn process(&mut self, x: &Vec<T>) -> Vec<T> {
         // Check we were initialised correctly and
         // ensure invariances required for unsafe code.
-        assert!(self.taps.len() > 0);
         assert!(self.decimate > 0);
-        assert!(self.decimate < self.delay.len());
+        assert!(self.interpolate > 0);
+        assert!(self.delay.len() > self.decimate);
+        assert!(self.taps.len() > self.interpolate);
+        assert!(self.taps.len() % self.interpolate == 0);
+        assert!(self.delay.len() == self.taps.len() / self.interpolate);
         assert!(self.delay_idx < self.delay.len() as isize);
-        assert_eq!(self.delay.len(), self.taps.len());
+        assert!(self.tap_idx < (self.taps.len() / self.interpolate) as isize);
         assert_eq!(x.len() % self.decimate, 0);
 
         // Allocate output
-        let mut y: Vec<T> = Vec::with_capacity(x.len() / self.decimate);
-        unsafe { y.set_len(x.len() / self.decimate) };
+        let mut y: Vec<T> = Vec::with_capacity(
+            (x.len() * self.interpolate) / self.decimate);
+        unsafe { y.set_len((x.len() * self.interpolate) / self.decimate) };
 
         // Grab pointers to various things
         let mut delay_idx = self.delay_idx;
+        let mut tap_idx = self.tap_idx;
         let delay_len = self.delay.len() as isize;
         let delay_p = &mut self.delay[0] as *mut T::AccType;
         let out_p = &mut y[0] as *mut T;
         let mut in_p = &x[0] as *const T;
         let ylen = y.len() as isize;
         let decimate = self.decimate as isize;
+        let interpolate = self.interpolate as isize;
         let tap0 = &self.taps[0] as *const T::TapType;
-        let gain: T::TapType = T::tap_sum();
+        let gain: T::TapType = T::gain(self.interpolate);
 
         // Process each actually generated output sample
         for k in 0..ylen {
 
-            // Feed the delay line, fast-forwarding through the skipped samples
+            // For every high-rate clock tick, advance the polyphase
+            // coefficient commutators by one, and when they wrap around,
+            // insert a new input into the delay line and advance that by one.
+            // Repeat until a sample we're not going to skip.
             for _ in 0..decimate {
-                unsafe {
-                    //*delay_p.offset(delay_idx) = SampleType::to_acc(*in_p);
-                    T::input(in_p, delay_p.offset(delay_idx));
-                    in_p = in_p.offset(1);
+                // Advance coefficient commutators.
+                // Note that the initialised value for tap_idx is
+                // interpolate - 1, so that on the first run we'll reset it
+                // to zero and add the first sample to the delay line.
+                tap_idx += 1;
+                if tap_idx == interpolate {
+                    tap_idx = 0;
+
+                    // Insert input sample
+                    unsafe {
+                        T::input(in_p, delay_p.offset(delay_idx));
+                        in_p = in_p.offset(1);
+                    }
                     delay_idx -= 1;
                     if delay_idx == -1 {
                         delay_idx = delay_len - 1;
@@ -214,35 +258,33 @@ impl <T: SampleType> FIR<T> {
                 }
             }
 
-            // Compute the multiply-accumulate for actual samples.
+            // Compute the multiply-accumulate only for output samples.
             let mut acc: T::AccType = T::AccType::zero();
-            let mut tap_p = tap0;
+            let mut tap_p = unsafe { tap0.offset(tap_idx) };
 
-            // First the index to the end of the buffer
+            // First compute from the index to the end of the buffer
             for idx in (delay_idx + 1)..delay_len {
                 unsafe {
-                    //acc = acc + T::tap_delay(*tap_p, *delay_p.offset(idx));
                     T::accumulate(&mut acc, delay_p.offset(idx), tap_p);
-                    tap_p = tap_p.offset(1);
+                    tap_p = tap_p.offset(interpolate);
                 }
             }
 
-            // Then the start to the index of the buffer
+            // Then compute from the start of the buffer to the index
             for idx in 0..(delay_idx + 1) {
                 unsafe {
-                    //acc = acc + T::tap_delay(*tap_p, *delay_p.offset(idx));
                     T::accumulate(&mut acc, delay_p.offset(idx), tap_p);
-                    tap_p = tap_p.offset(1);
+                    tap_p = tap_p.offset(interpolate);
                 }
             }
 
             // Save the result, accounting for filter gain
-            //unsafe { *out_p.offset(k) = T::scale_acc(acc, gain) };
             unsafe { T::output(&acc, out_p.offset(k), &gain); }
         }
 
         // Update index for next time
         self.delay_idx = delay_idx;
+        self.tap_idx = tap_idx;
 
         y
     }
@@ -338,7 +380,7 @@ mod tests {
     #[test]
     fn test_fir_impulse() {
         let taps: Vec<i32> = vec!{8192, 16384, 8192};
-        let mut fir = FIR::new(&taps, 1);
+        let mut fir = FIR::new(&taps, 1, 1);
         let x: Vec<i16> = vec!{4, 0, 0, 0, 0, 0, 0};
         let y = fir.process(&x);
         assert_eq!(y, vec!{1, 2, 1, 0, 0, 0, 0});
@@ -347,7 +389,7 @@ mod tests {
     #[test]
     fn test_fir_impulse_f() {
         let taps: Vec<f64> = vec!{0.25, 0.5, 0.25};
-        let mut fir = FIR::new(&taps, 1);
+        let mut fir = FIR::new(&taps, 1, 1);
         let x: Vec<f32> = vec!{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
         let y = fir.process(&x);
         assert_eq!(y, vec!{0.25, 0.5, 0.25, 0.0, 0.0, 0.0, 0.0});
@@ -356,7 +398,7 @@ mod tests {
     #[test]
     fn test_fir_impulse_c() {
         let taps: Vec<i32> = vec!{8192, 16384, 8192};
-        let mut fir = FIR::new(&taps, 1);
+        let mut fir = FIR::new(&taps, 1, 1);
         let x: Vec<Complex<i16>> = vec!{
             Complex{re:4, im:-8},
             Complex{re:0, im: 0},
@@ -376,7 +418,7 @@ mod tests {
     #[test]
     fn test_fir_impulse_cf() {
         let taps: Vec<f64> = vec!{0.25, 0.5, 0.25};
-        let mut fir = FIR::new(&taps, 1);
+        let mut fir = FIR::new(&taps, 1, 1);
         let x: Vec<Complex<f32>> = vec!{
             Complex{re:1.0, im:-2.0},
             Complex{re:0.0, im: 0.0},
@@ -396,16 +438,35 @@ mod tests {
     #[test]
     fn test_fir_decimate() {
         let taps: Vec<i32> = vec!{8192, 8192, 8192, 8192};
-        let mut fir = FIR::new(&taps, 2);
+        let mut fir = FIR::new(&taps, 2, 1);
         let x: Vec<i16> = vec!{4, 4, 4, 4, 8, 8, 8, 8};
         let y = fir.process(&x);
         assert_eq!(y, vec!{2, 4, 6, 8});
     }
 
     #[test]
+    fn test_fir_interpolate() {
+        let taps: Vec<i32> = vec!{
+            -55, 0, 96, 0, -220, 0, 461, 0, -877, 0, 1608, 0, -3176, 0, 10342,
+            16410, 10342, 0, -3176, 0, 1608, 0, -877, 0, 461, 0, -220, 0, 96,
+            0, -55};
+        let mut fir = FIR::new(&taps, 1, 2);
+        let x: Vec<i16> = vec!{
+            10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150,
+            160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280,
+            290, 300};
+        let y = fir.process(&x);
+        assert_eq!(y, vec!{
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 3, 10, 15, 20, 24, 30,
+            34, 40, 44, 50, 54, 60, 64, 70, 74, 80, 84, 90, 94, 100, 104, 110,
+            114, 120, 124, 130, 134, 140, 144, 150, 154, 160, 164, 170, 174,
+            180, 184, 190, 194, 200, 204, 210, 214, 220, 224, 230});
+    }
+
+    #[test]
     fn test_fir_continuity() {
         let taps: Vec<i32> = vec!{8192, 16384, 8192};
-        let mut fir = FIR::new(&taps, 1);
+        let mut fir = FIR::new(&taps, 1, 1);
         let x: Vec<i16> = vec!{4, 0};
         let y = fir.process(&x);
         assert_eq!(y, vec!{1, 2});

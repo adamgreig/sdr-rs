6adc08071ca75b8d4719e27f6ba006a08a3da844
Adam Greig <adam@adamgreig.com>
2015-09-14 03:38:07 +0100

    Have FM demodulator maintain differentiator state across calls

--- src/fm.rs
+++ src/fm.rs
@@ -2,44 +2,74 @@ use num::{Complex, Num, Zero};
 
 pub type IQ<T> = Complex<T>;
 
-pub trait SampleType: Copy + Num + Zero {}
-impl SampleType for i8 {}
-impl SampleType for i16 {}
-impl SampleType for i32 {}
-impl SampleType for f32 {}
-impl SampleType for f64 {}
+/// A trait for types the FM Demodulator can process.
+/// Always IQ<T> in and <T> out.
+pub trait SampleType: Copy + Num + Zero {
+    fn small() -> Self;
+}
+impl SampleType for i8  { fn small() -> i8  { 1 }}
+impl SampleType for i16 { fn small() -> i16 { 1 }}
+impl SampleType for i32 { fn small() -> i32 { 1 }}
+impl SampleType for f32 { fn small() -> f32 { 1e-6 }}
+impl SampleType for f64 { fn small() -> f64 { 1e-6 }}
 
-pub fn fm_demod<T: SampleType>(x: &Vec<IQ<T>>) -> Vec<T> {
-    let n = x.len();
-    let mut d: [IQ<T>; 3] = [IQ::new(T::zero(), T::zero()); 3];
-    let mut out: Vec<T> = Vec::with_capacity(n);
-    unsafe { out.set_len(n) };
-    
-    let xp = &x[0] as *const IQ<T>;
-    let op = &mut out[0] as *mut T;
-    let d0 = &mut d[0] as *mut IQ<T>;
-    let d1 = &mut d[1] as *mut IQ<T>;
-    let d2 = &mut d[2] as *mut IQ<T>;
-    let d0re = &d[0].re as *const T;
-    let d1re = &d[1].re as *const T;
-    let d2re = &d[2].re as *const T;
-    let d0im = &d[0].im as *const T;
-    let d1im = &d[1].im as *const T;
-    let d2im = &d[2].im as *const T;
+/// FM signal demodulator
+pub struct FMDemod<T: SampleType> {
+    d: [IQ<T>; 3]
+}
 
-    unsafe { *d0 = *xp; *d1 = *xp; }
-
-    for i in 0isize..(n as isize) {
-        unsafe {
-            *d2 = *d1;
-            *d1 = *d0;
-            *d0 = *xp.offset(i);
-            *op.offset(i) = (*d1re * (*d0im - *d2im) - *d1im * (*d0re - *d2re))
-                            / (*d1re * *d1re + *d1im * *d1im);
-        }
+impl <T:SampleType> FMDemod<T> {
+    /// Create a new FM demodulator
+    pub fn new() -> FMDemod<T> {
+        let mut d: [IQ<T>; 3] = [IQ::new(T::zero(), T::zero()); 3];
+        FMDemod{d: d}
     }
 
-    out
+    /// FM demodulate input block x, containing baseband IQ data that has been
+    /// frequency modulated. Outputs a block of real-valued samples.
+    pub fn process(&mut self, x: &Vec<IQ<T>>) -> Vec<T> {
+        let n = x.len();
+        let mut d = self.d;
+
+        // Allocate output
+        let mut out: Vec<T> = Vec::with_capacity(n);
+        unsafe { out.set_len(n) };
+
+        // Grab pointers to everything for speedy access
+        let xp = &x[0] as *const IQ<T>;
+        let op = &mut out[0] as *mut T;
+        let d0 = &mut d[0] as *mut IQ<T>;
+        let d1 = &mut d[1] as *mut IQ<T>;
+        let d2 = &mut d[2] as *mut IQ<T>;
+        let d0re = &d[0].re as *const T;
+        let d1re = &d[1].re as *const T;
+        let d2re = &d[2].re as *const T;
+        let d0im = &d[0].im as *const T;
+        let d1im = &d[1].im as *const T;
+        let d2im = &d[2].im as *const T;
+        let small = T::small();
+
+        // FM demodulate. Instantaneous phase is approximated by
+        // a three-long differentiator doing a multiplication with the
+        // old conjugate signal, around the point of interest.
+        //
+        // Normalised by incoming signal magnitude, but this can go wrong
+        // for zero valued signals so we also add a "tiny" delta.
+        for i in 0isize..(n as isize) {
+            unsafe {
+                *d2 = *d1;
+                *d1 = *d0;
+                *d0 = *xp.offset(i);
+                *op.offset(i) = (*d1re * (*d0im - *d2im) - *d1im * (*d0re - *d2re))
+                                / (*d1re * *d1re + *d1im * *d1im + small);
+            }
+        }
+
+        // Save differentiator state for next time
+        self.d = d;
+
+        out
+    }
 }
 
 #[test]

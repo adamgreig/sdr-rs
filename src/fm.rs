54804df1954e801b683826219036421b8a577387
Adam Greig <adam@adamgreig.com>
2015-09-15 04:15:05 +0100

    Improvements to FM demodulator

--- src/fm.rs
+++ src/fm.rs
@@ -5,23 +5,83 @@ pub type IQ<T> = Complex<T>;
 /// A trait for types the FM Demodulator can process.
 /// Always IQ<T> in and <T> out.
 pub trait SampleType: Copy + Num + Zero {
-    fn small() -> Self;
+    type Reg: Copy + Num + Zero;
+    unsafe fn set_reg(x: *const Complex<Self>, reg: *mut Complex<Self::Reg>);
+    unsafe fn set_out(d0re: *const Self::Reg, d0im: *const Self::Reg,
+                      d1re: *const Self::Reg, d1im: *const Self::Reg,
+                      d2re: *const Self::Reg, d2im: *const Self::Reg,
+                      op: *mut Self);
 }
-impl SampleType for i8  { fn small() -> i8  { 1 }}
-impl SampleType for i16 { fn small() -> i16 { 1 }}
-impl SampleType for i32 { fn small() -> i32 { 1 }}
-impl SampleType for f32 { fn small() -> f32 { 1e-6 }}
-impl SampleType for f64 { fn small() -> f64 { 1e-6 }}
+
+macro_rules! impl_fixed_sampletype {
+    ($t: ty, $tt: ty, $bits: expr) => {
+        impl SampleType for $t {
+            type Reg = $tt;
+
+            #[inline]
+            unsafe fn set_reg(x: *const Complex<$t>, reg: *mut Complex<$tt>) {
+                (*reg).re = (*x).re as $tt;
+                (*reg).im = (*x).im as $tt;
+            }
+
+            #[inline]
+            unsafe fn set_out(d0re: *const Self::Reg, d0im: *const Self::Reg,
+                              d1re: *const Self::Reg, d1im: *const Self::Reg,
+                              d2re: *const Self::Reg, d2im: *const Self::Reg,
+                              op: *mut Self) {
+                let top = *d1re * (*d0im - *d2im) - *d1im * (*d0re - *d2re);
+                let bottom = (*d1re * *d1re + *d1im * *d1im) >> $bits;
+                if bottom == 0 {
+                    *op = top as $t;
+                } else {
+                    *op = (top / bottom) as $t;
+                }
+            }
+        }
+    }
+}
+
+macro_rules! impl_floating_sampletype {
+    ($t: ty) => {
+        impl SampleType for $t {
+            type Reg = $t;
+            #[inline]
+            unsafe fn set_reg(x: *const Complex<$t>, reg: *mut Complex<$t>) {
+                *reg = *x;
+            }
+
+            #[inline]
+            unsafe fn set_out(d0re: *const Self::Reg, d0im: *const Self::Reg,
+                              d1re: *const Self::Reg, d1im: *const Self::Reg,
+                              d2re: *const Self::Reg, d2im: *const Self::Reg,
+                              op: *mut Self) {
+                let top = *d1re * (*d0im - *d2im) - *d1im * (*d0re - *d2re);
+                let bottom = *d1re * *d1re + *d1im * *d1im;
+                if bottom == 0.0 {
+                    *op = top;
+                } else {
+                    *op = top / bottom;
+                }
+            }
+        }
+    }
+}
+
+impl_fixed_sampletype!(i8, i16, 8-2);
+impl_fixed_sampletype!(i16, i32, 16-2);
+impl_fixed_sampletype!(i32, i64, 32-2);
+impl_floating_sampletype!(f32);
+impl_floating_sampletype!(f64);
 
 /// FM signal demodulator
 pub struct FMDemod<T: SampleType> {
-    d: [IQ<T>; 3]
+    d: [IQ<T::Reg>; 3]
 }
 
 impl <T:SampleType> FMDemod<T> {
     /// Create a new FM demodulator
     pub fn new() -> FMDemod<T> {
-        let d: [IQ<T>; 3] = [IQ::new(T::zero(), T::zero()); 3];
+        let d: [IQ<T::Reg>; 3] = [IQ::new(T::Reg::zero(), T::Reg::zero()); 3];
         FMDemod{d: d}
     }
 
@@ -38,16 +98,15 @@ impl <T:SampleType> FMDemod<T> {
         // Grab pointers to everything for speedy access
         let xp = &x[0] as *const IQ<T>;
         let op = &mut out[0] as *mut T;
-        let d0 = &mut d[0] as *mut IQ<T>;
-        let d1 = &mut d[1] as *mut IQ<T>;
-        let d2 = &mut d[2] as *mut IQ<T>;
-        let d0re = &d[0].re as *const T;
-        let d1re = &d[1].re as *const T;
-        let d2re = &d[2].re as *const T;
-        let d0im = &d[0].im as *const T;
-        let d1im = &d[1].im as *const T;
-        let d2im = &d[2].im as *const T;
-        let small = T::small();
+        let d0 = &mut d[0] as *mut IQ<T::Reg>;
+        let d1 = &mut d[1] as *mut IQ<T::Reg>;
+        let d2 = &mut d[2] as *mut IQ<T::Reg>;
+        let d0re = &d[0].re as *const T::Reg;
+        let d1re = &d[1].re as *const T::Reg;
+        let d2re = &d[2].re as *const T::Reg;
+        let d0im = &d[0].im as *const T::Reg;
+        let d1im = &d[1].im as *const T::Reg;
+        let d2im = &d[2].im as *const T::Reg;
 
         // FM demodulate. Instantaneous phase is approximated by
         // a three-long differentiator doing a multiplication with the
@@ -59,9 +118,8 @@ impl <T:SampleType> FMDemod<T> {
             unsafe {
                 *d2 = *d1;
                 *d1 = *d0;
-                *d0 = *xp.offset(i);
-                *op.offset(i) = (*d1re * (*d0im - *d2im) - *d1im * (*d0re - *d2re))
-                                / (*d1re * *d1re + *d1im * *d1im + small);
+                T::set_reg(xp.offset(i), d0);
+                T::set_out(d0re, d0im, d1re, d1im, d2re, d2im, op.offset(i));
             }
         }
 
